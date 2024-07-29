88f386a034759f616d296b562c8fcb005561be4c
l3olvy <qhsl0615@naver.com>
2024-07-30 08:40:51 +0900

    [Silver II] Title: 부분수열의 합, Time: 132 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -3,39 +3,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-	public static int n, s;
-	public static int[] arr;
-	public static int count = 0;
-	
-	public static void dfs(int start, int sum) {
-		if(start >= n)
-			return;
-		sum += arr[start];
-		if(sum == s) {
-			++count;
-		}
-		
-		dfs(start + 1, sum);
-		dfs(start + 1, sum - arr[start]);
-	}
+public class Main{
+    public static int n, s;
+    public static int[] arr;
+    public static int count = 0;
 
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		s = Integer.parseInt(st.nextToken());
-		
-		arr = new int[n];
-		
-		st = new StringTokenizer(br.readLine());
-		
-		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		dfs(0, 0);
-		System.out.println(count);
-	}
+    public static void dfs(int start, int sum) {
+        // 중간 과정에서 목표 합에 도달했을 때도 체크
+        if(sum == s) {
+            count++;
+        }
+        
+        for(int i = start; i < n; i++) {
+            dfs(i + 1, sum + arr[i]);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dfs(0, 0);
+
+        // 부분 수열의 크기가 0인 경우는 세지 않기 때문에 count를 조정
+        if (s == 0) {
+            count--;
+        }
+        
+        System.out.println(count);
+    }
 }
