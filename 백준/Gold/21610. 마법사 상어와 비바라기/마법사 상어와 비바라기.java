9b0b8fe6595609ee049d5676521cb50a91d3fec4
l3olvy <qhsl0615@naver.com>
2024-09-14 19:53:00 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 140 ms, Memory: 21072 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
@@ -1,134 +1,102 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
 
 public class Main {
+		static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		static StringBuilder sb = new StringBuilder();
+		static StringTokenizer st;
 
-	static int N, M, map[][], sumWater;
-	static ArrayList<Node> clouds;
-	static boolean prevCloud[][];
-	
-	// 이동방향 ←, ↖, ↑, ↗, →, ↘, ↓, ↙
-	static int dx[] = {0, 0, -1, -1, -1, 0, 1, 1, 1};
-	static int dy[]	= {0, -1, -1, 0, 1, 1, 1, 0, -1};
-	
-	public static class Node {
-		int x, y;
-		
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
+		static Deque<int[]> cloud = new ArrayDeque<>();
+		static int N, M, A[][];
+		static int[] dr = { 0, 0, -1, -1, -1, 0, 1, 1, 1 }, dc = { 0, -1, -1, 0, 1, 1, 1, 0, -1 }, nearR = { -1, -1, 1, 1 },
+				nearC = { -1, 1, -1, 1 };
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		map = new int[N][N];
-		
-		for (int r = 0; r < N; r++) {
-			st = new StringTokenizer(br.readLine());
-			for (int c = 0; c < N; c++) {
-				map[r][c] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		clouds = new ArrayList<Node>();
-		
-		// 초기 비바라기 마법 시전
-		clouds.add(new Node(N - 1, 0));
-		clouds.add(new Node(N - 1, 1));
-		clouds.add(new Node(N - 2, 0));
-		clouds.add(new Node(N - 2, 1));
-		
-		prevCloud = new boolean[N][N];
+		public static void main(String[] args) throws IOException {
+			st = new StringTokenizer(in.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			A = new int[N][N];
 
-		// M번 명령에 따른 비바라기 마법 시전
-		for (int m = 0; m < M; m++) {
-			st = new StringTokenizer(br.readLine());
-			int d = Integer.parseInt(st.nextToken());
-			int s = Integer.parseInt(st.nextToken());
-
-			moveCloud(d, s);
-			copyWater();
-			makeCloud();
-		}
-		
-		// 바구니에 들어있는 물의 양의 합 구한 후 출력
-		sumWater = 0;
-		for (int r = 0; r < N; r++) {
-			for (int c = 0; c < N; c++) {
-				sumWater += map[r][c];
-			}
-		}
-		
-		System.out.println(sumWater);
-	}
-	
-	// 구름 이동 & 비내림 물 +1 증가
-	public static void moveCloud(int d, int s) {
-		
-		for (int i = 0; i < clouds.size(); i++) {
-			Node cloud = clouds.get(i);
-			
-			// 구름 이동
-			int nx = cloud.x + dx[d] * s;
-			int ny = cloud.y + dy[d] * s;
-
-			if (nx >= N) nx %= N;
-			else if (nx < 0) nx = -((-nx) % N) == 0 ? 0 : -((-nx) % N) + N;
-			if (ny >= N) ny %= N;
-			else if (ny < 0) ny = -((-ny) % N) == 0 ? 0 : -((-ny) % N) + N;
-
-			cloud.x = nx;
-			cloud.y = ny;
-			
-			// 비내림 물 +1 증가
-			prevCloud[cloud.x][cloud.y] = true;
-			map[cloud.x][cloud.y] += 1;
-		}
-	}
-	
-	// 물복사버그 마법
-	public static void copyWater() {
-		for (int i = 0; i < clouds.size(); i++) {
-			Node cloud = clouds.get(i);
-			int count = 0;
-
-			for (int d = 2; d <= 8; d += 2) {
-				int nx = cloud.x + dx[d];
-				int ny = cloud.y + dy[d];
-				// 범위 넘어가면 pass
-				if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
-					continue;
+			for (int r = 0; r < N; r++) {
+				st = new StringTokenizer(in.readLine());
+				for (int c = 0; c < N; c++) {
+					A[r][c] = Integer.parseInt(st.nextToken());
 				}
-				
-				if (map[nx][ny] > 0) count++;
-				
 			}
 
-			map[cloud.x][cloud.y] += count;
-		}
-		
-		// 이전 구름 삭제
-		clouds.removeAll(clouds);
-	}
-	
-	// 물의 양 2 이상인 칸에 구름 생성 후 물 -2 감소
-	public static void makeCloud() {
-		for (int r = 0; r < N; r++) {
-			for (int c = 0; c < N; c++) {
-				if (map[r][c] >= 2 && !prevCloud[r][c]) {
-					clouds.add(new Node(r, c));
-					map[r][c] -= 2;
-					prevCloud[r][c] = true;
+			// 초반 4개 구름 생성
+			for (int r = N - 1; r > N - 3; r--) {
+				for (int c = 0; c < 2; c++) {
+
+					cloud.add(new int[] { r, c });
 				}
-				if (prevCloud[r][c])
-					prevCloud[r][c] = false;
 			}
+
+			// M번 이동할 때마다 다음 과정 반복
+			for (int m = 0; m < M; m++) {
+				st = new StringTokenizer(in.readLine());
+				int d = Integer.parseInt(st.nextToken());
+				int s = Integer.parseInt(st.nextToken());
+				boolean[][] wasCloud = new boolean[N][N];
+
+				// 1. move cloud & add water
+				for (int[] c : cloud) {
+					
+					int nx = c[0] + dr[d] * s;
+					int ny = c[1] + dc[d] * s;
+
+					if (nx >= N) nx %= N;
+					else if (nx < 0) nx = -((-nx) % N) == 0 ? 0 : -((-nx) % N) + N;
+					if (ny >= N) ny %= N;
+					else if (ny < 0) ny = -((-ny) % N) == 0 ? 0 : -((-ny) % N) + N;
+
+					c[0] = nx;
+					c[1] = ny;
+					A[c[0]][c[1]]++;
+					wasCloud[c[0]][c[1]] = true;
+				}
+
+				// 2. add water (마법)
+				while (!cloud.isEmpty()) {
+					int[] c = cloud.poll();
+
+					int row = c[0];
+					int col = c[1];
+
+					for (int i = 0; i < 4; i++) {
+						int nr = row + nearR[i];
+						int nc = col + nearC[i];
+						if (nr < 0 || nr >= N || nc < 0 || nc >= N || A[nr][nc] <= 0)
+							continue;
+						A[row][col]++;
+
+					}
+				}
+
+
+				// 3. find new cloud
+				for (int r = 0; r < N; r++) {
+					for (int c = 0; c < N; c++) {
+						if (A[r][c] > 1 && !wasCloud[r][c]) {
+							A[r][c] -= 2;
+							cloud.add(new int[] { r, c });
+						}
+					}
+				}
+			}
+
+			int sum = 0;
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < N; c++) {
+					sum += A[r][c];
+				}
+			}
+			System.out.println(sum);
+
 		}
 	}
-}
