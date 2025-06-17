ac54a37861246a379e77308621dfca4908fbba69
l3olvy <qhsl0615@naver.com>
2025-06-17 17:47:47 +0900

    [level 3] Title: 입국심사, Time: 116.42 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 103 MB, 시간: 132.19 ms
+메모리: 84.9 MB, 시간: 116.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 07일 23:18:07
+2025년 06월 17일 17:47:42
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,23 +2,26 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
         Arrays.sort(times);
-        long left = 0;
-        long right = times[times.length-1] * (long)n; 
-        
-        while(left <= right) {
-            long mid = (left + right) / 2;
-            long complete = 0;
-            for (int i = 0; i < times.length; i++)
-                complete += mid / times[i];
-            if (complete < n)
-                left = mid + 1;
+        long answer = 0;
+        long start = 0;
+        long end = times[times.length - 1] * (long) n;
+
+        while (start <= end) {
+            long sum = 0;
+            long mid = (start + end) / 2;
+            for (int i = 0; i < times.length; i++) {
+                sum += mid / times[i];
+            }
+            if (sum < n) {
+                start = mid + 1;
+            } 
             else {
-                right = mid - 1;
+                end = mid - 1;
                 answer = mid;
             }
-        }  
+        }
+        
         return answer;
     }
 }
