461b6ec022ce3fa537ab76594cc20ed7ed22196a
l3olvy <qhsl0615@naver.com>
2025-07-08 21:33:58 +0900

    [Gold IV] Title: 물과 응애, Time: 184 ms, Memory: 18772 KB -BaekjoonHub

---
+++ 백준/Gold/33983. 물과 응애/물과 응애.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+    static int N, H, O;
+    static char arr[];
+    
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+	    N = Integer.parseInt(st.nextToken());
+	    st = new StringTokenizer(br.readLine());
+	    arr = st.nextToken().toCharArray();
+	    
+	    if (N % 3 != 0) {
+	        System.out.print("mix");
+	        return;
+	    }
+	    
+	    for (int i = 0; i < arr.length; i++) {
+	        if (arr[i] == 'H') H++;
+	        else O++;
+	        
+	        if ((i < arr.length - 1 && H - O > (N / 3))) {
+	            System.out.print("mix");
+	            return;
+	        }
+	        if (O > H) {
+	            System.out.print("mix");
+	            return;
+	        }
+	    }
+	    
+	    if (H != 2 * O) {
+	        System.out.print("mix");
+	        return;
+	    } else {
+	        System.out.print("pure");
+	    }
+	}
+}
